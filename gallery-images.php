cf91a07f0d8ecaa93794f4f35792b5e634df4e9f
Narek Saghatelyan <n.saghatelyan@gmail.com>
2017-05-29 18:27:46 +0400

    integrate hover effects

--- gallery-images.php
+++ gallery-images.php
@@ -4,8 +4,8 @@
 Plugin Name: Huge IT Image Gallery
 Plugin URI: https://huge-it.com/wordpress-gallery/
 Description: Gallery image is the best gallery plugin to use if you want to be original with your website. Responsive image gallery with many views.
-Version: 2.1.7
-Author: Photo Gallery by Huge-IT
+Version: 2.2.0
+Author: Huge-IT
 Author URI: https://huge-it.com/
 License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -24,7 +24,7 @@ if ( ! class_exists( 'Gallery_Img' ) ) :
          * Version of plugin
          * @var float
          */
-        public $version = '2.1.7';
+        public $version = '2.2.0';
 
         /**
          * Instance of Gallery_Img_Admin class to manage admin
@@ -150,7 +150,7 @@ if ( ! class_exists( 'Gallery_Img' ) ) :
                 include_once( 'includes/admin/gallery-img-admin-functions.php' );
             }
             if ( $this->is_request( 'frontend' ) ) {
-                $this->frontend_includes();
+                    $this->frontend_includes();
             }
 
         }
